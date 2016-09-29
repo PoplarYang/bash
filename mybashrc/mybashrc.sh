0364551704d35da41d5861f599fbebf85c4e3291
hiyang <echohiyang@foxmail.com>
2016-09-29 11:36:46 +0800

    add svn script

--- mybashrc/mybashrc.sh
+++ mybashrc/mybashrc.sh
@@ -2,7 +2,7 @@
 alias gonet='cd /etc/sysconfig/network-scripts/'
 alias vimeth0='vim /etc/sysconfig/network-scripts/ifcfg-eth0'
 alias sernet='service network'
-
+alias netl='netstat -tlnp'
 # about yum
 alias yumin='yum install'
 alias yumloin='yum localinstall'
