319b9fe2a86161bec77507907428404a0d561be9
hiyang <echohiyang@foxmail.com>
2016-09-29 13:26:56 +0800

    modify

--- mybashrc/mybashrc.sh
+++ mybashrc/mybashrc.sh
@@ -3,6 +3,11 @@ alias gonet='cd /etc/sysconfig/network-scripts/'
 alias vimeth0='vim /etc/sysconfig/network-scripts/ifcfg-eth0'
 alias sernet='service network'
 alias netl='netstat -tlnp'
+alias ipc='ifconfig'
+
+# about iptables
+alias iptl='iptables -nvL --line-numbers'
+
 # about yum
 alias yumin='yum install'
 alias yumloin='yum localinstall'
@@ -15,7 +20,7 @@ alias anplay='ansible-playbook'
 alias gitlog='git log'
 alias gita='git add'
 alias gitc='git commit -m'
-
+alias gitpush='git push -u origin master'
 # about ntpdate
 alias ntime='ntpdate cn.pool.ntp.org'
 
